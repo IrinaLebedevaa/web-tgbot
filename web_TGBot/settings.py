56f8dc7466152d1cf167b00b742e2f20fc4fb348
Your Name <you@example.com>
2026-02-04 23:07:37 +0500

    1

--- web_TGBot/settings.py
+++ web_TGBot/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-19^b)gcw^oy3z#a4wto)9e-3pvrz!fv(ev)ca5m&i$6!a*^&wu
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # =======================
 # Приложения
