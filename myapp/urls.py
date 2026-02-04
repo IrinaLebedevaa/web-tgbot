55b5b6d1dd5b83b90a8072e5d0671e54a0ade285
Your Name <you@example.com>
2026-02-05 01:29:21 +0500

    1

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, ozon1
 
 urlpatterns = [
     path('', home, name='home'),  # главная страница
+    path('ozon1', ozon1, name='ozon1'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')  # здесь открываем html
+
+def ozon1(request):
+    return render(request, 'ozon1.html')  # здесь открываем html
