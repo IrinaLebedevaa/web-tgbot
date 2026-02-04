96abfd0298e06391e26b71167b0ecd603a4f144a
Your Name <you@example.com>
2026-02-05 01:50:24 +0500

    1

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,5 @@ from .views import home, ozon2
 
 urlpatterns = [
     path('', home, name='home'),  # главная страница
-    path('ozon2', ozon2, name='ozon2'),
+    path('ozon2/', ozon2, name='ozon2'),
 ]
